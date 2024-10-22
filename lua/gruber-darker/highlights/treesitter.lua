cf19b4ec41348d2fdfe28aa2654e92f9db237506
Fabiano Barbosa <fabiano.siqueirab@gmail.com>
2024-10-22 15:57:43 -0300

    Update treesitter.lua

--- lua/gruber-darker/highlights/treesitter.lua
+++ lua/gruber-darker/highlights/treesitter.lua
@@ -138,9 +138,9 @@ M.highlights.storage_class = Highlight.new("@storageclass", { link = vim_hl.stor
 -- I don't think this is supported anymore...
 -- M.highlights.attribute = Highlight.new("@attribute", { link = gruber_hl.brown })
 ---object and struct fields
-M.highlights.field = Highlight.new("@field", { link = gruber_hl.niagara })
+M.highlights.field = Highlight.new("@field", { link = gruber_hl.yellow })
 ---similar to `@field`
-M.highlights.property = Highlight.new("@property", { link = gruber_hl.dark_niagara })
+M.highlights.property = Highlight.new("@property", { link = gruber_hl.yellow })
 
 -- Identifiers
 
