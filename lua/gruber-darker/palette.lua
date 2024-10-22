936c4486ddae85378cb7ba9382d5ca3baa5fa89c
Fabiano Barbosa <fabiano.siqueirab@gmail.com>
2024-10-22 16:03:12 -0300

    Update palette.lua

--- lua/gruber-darker/palette.lua
+++ lua/gruber-darker/palette.lua
@@ -3,29 +3,23 @@ local M = {}
 
 ---@type table<string, Color>
 M = {
-	none = Color.none(),
-	fg = Color.new(0xe4e4e4),
-	["fg+1"] = Color.new(0xf4f4ff),
-	["fg+2"] = Color.new(0xf5f5f5),
-	white = Color.new(0xffffff),
-	black = Color.new(0x000000),
-	["bg-1"] = Color.new(0x101010),
-	bg = Color.new(0x181818),
-	["bg+1"] = Color.new(0x282828),
-	["bg+2"] = Color.new(0x453d41),
-	["bg+3"] = Color.new(0x484848),
-	["bg+4"] = Color.new(0x52494e),
-	["red-1"] = Color.new(0xc73c3f),
-	red = Color.new(0xf43841),
-	["red+1"] = Color.new(0xff4f58),
-	green = Color.new(0x73d936),
-	yellow = Color.new(0xffdd33),
-	brown = Color.new(0xcc8c3c),
-	quartz = Color.new(0x95a99f),
-	["niagara-2"] = Color.new(0x303540),
-	["niagara-1"] = Color.new(0x565f73),
-	niagara = Color.new(0x96a6c8),
-	wisteria = Color.new(0x9e95c7),
+    none = Color.none(),
+    fg = Color.new(0xe4e4e4), -- Keeping the original foreground color
+    ["fg+1"] = Color.new(0xf4f4ff),
+    ["fg+2"] = Color.new(0xf5f5f5),
+    white = Color.new(0xffffff),
+    black = Color.new(0x000000),
+    ["bg-1"] = Color.new(0x333333), -- Dark tone
+    bg = Color.new(0x181818),
+    ["bg+1"] = Color.new(0x282828),
+    ["bg+2"] = Color.new(0x454545), -- Adjusted based on the palette
+    ["bg+3"] = Color.new(0x3a3a3a),
+    ["bg+4"] = Color.new(0x444444),
+    blue1 = Color.new(0x4a90e2), -- Light blue
+    blue2 = Color.new(0x5fa8e5), -- Slightly brighter blue
+    yellow1 = Color.new(0xffeb99), -- Light yellow
+    yellow2 = Color.new(0xffcc00), -- Bright yellow
+    dark_gray = Color.new(0x333333),
 }
 
 return M
